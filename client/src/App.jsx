a82e523cc43dfe994cc28d8157eee73edfd3c428
Nabemh <nhanior@gmail.com>
2025-04-04 00:47:09 +0100

    Pages setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,12 @@ const App = () => {
 
       <div className="flex-1 max:sm:w-full max-w-[1280px] mx-auto sm:pr-5">
         Navbar
+
+        <Routes>
+          <Route path='/' element={ <Home /> } />
+        </Routes>
       </div>
+
     </div>
   )
 }
---
+++ client/src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './Home';
+export { default as profile } from './Profile';
+export { default as CreateCampaign } from './CreateCampaign';
+export { default as CampaignDetails } from './CampaignDetails';
