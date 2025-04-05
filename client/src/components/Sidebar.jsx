3b2d6d65e2442cbb13470160f038f6cdf22129c7
Nabemh <nhanior@gmail.com>
2025-04-05 17:12:26 +0100

    icon fix

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -5,10 +5,11 @@ import { navlinks } from '../constants';
 import CampaignDetails from '../pages/CampaignDetails'
 
 const Icon = ({ styles, name, imgUrl, isActive, disabled, handleClick }) => (
-  <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#2c2f32]'} flex jsutify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`} onClick={handleClick}>
+  <div className={`w-[48px] h-[48px] rounded-[10px] ${isActive && isActive === name && 'bg-[#2c2f32]'} flex justify-center items-center ${!disabled && 'cursor-pointer'} ${styles}`} onClick={handleClick}>
     {!isActive ? (
-      <img src={imgUrl} alt="fund_logo" className='w-1/2 h-1/2'/>
-    ): (<img src={imgUrl} alt="fund_logo" className={`w-1/2 h-1/2 ${isActive !== name && 'grayscale'}`}/>
+      <img src={imgUrl} alt="fund_logo" className="w-1/2 h-1/2" />
+    ) : (
+      <img src={imgUrl} alt="fund_logo" className={`w-1/2 h-1/2 ${isActive !== name && 'grayscale'}`} />
     )}
   </div>
 )
@@ -23,7 +24,7 @@ const Sidebar = () => {
       </Link>
 
       <div>
-        
+
       </div>
     </div>
   )
