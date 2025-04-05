fa37c309e2a9d2b0aaf02d1519242115069a6eb2
Nabemh <nhanior@gmail.com>
2025-04-05 20:03:24 +0100

    feat: create campaign

--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -18,40 +18,88 @@ const CreateCampaign = () => {
     image: '',
   });
 
-  const handleSubmit = () => {
+  const handleFormChange = (fieldName, e) => {
+    setForm({ ...form, [fieldName] : e.target.value })
+  }
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
   }
 
   return (
-    <div className='bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4'>
-      {isLoading && 'Loading...'}
-      <div className='flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#3a3a43] rounded-[10px]'>
-        <h1 className='font-epilogue font-bold sm:text-[18px] leading-[38px] text-white'>
-          Start a Campaign
-        </h1>
+    <div className="bg-[#1c1c24] flex justify-center items-center flex-col rounded-[10px] sm:p-10 p-4">
+      {isLoading && <Loader />}
+      <div className="flex justify-center items-center p-[16px] sm:min-w-[380px] bg-[#3a3a43] rounded-[10px]">
+        <h1 className="font-epilogue font-bold sm:text-[25px] text-[18px] leading-[38px] text-white">Start a Campaign</h1>
       </div>
 
-      <form onSubmit={handleSubmit} className='w-full mt-[65px] flex flex-col gap-[30px'>
-        <div className='flex flex-wrap gap-[40px]'>
-            <FormField
-              labelName="Your Name *"
-              placeholder="John Doe"
-              inputType="text"
-              value={form.name}
-              handleChange={() => {}}
-            />
+      <form onSubmit={handleSubmit} className="w-full mt-[65px] flex flex-col gap-[30px]">
 
-            <FormField
-              labelName="Campaign title *"
-              placeholder="Write a title"
-              inputType="text"
-              value={form.title}
-              handleChange={() => {}}
-            />
+        <div className="flex flex-wrap gap-[40px]">
+          <FormField
+            labelName="Your Name *"
+            placeholder="John Doe"
+            inputType="text"
+            value={form.name}
+            handleChange={(e) => handleFormChange('name', e)}
+          />
+          <FormField
+            labelName="Campaign Title *"
+            placeholder="Write a title"
+            inputType="text"
+            value={form.title}
+            handleChange={(e) => handleFormChange('title', e)}
+          />
         </div>
 
-      </form>
+        <FormField
+            labelName="Story *"
+            placeholder="Write your story"
+            isTextArea
+            value={form.description}
+            handleChange={(e) => handleFormChange('description', e)}
+          />
 
+        <div className="w-full flex justify-start items-center p-4 bg-[#8c6dfd] h-[120px] rounded-[10px]">
+          <img src={money} alt="money" className="w-[40px] h-[40px] object-contain"/>
+          <h4 className="font-epilogue font-bold text-[25px] text-white ml-[20px]">You will get 100% of the raised amount</h4>
+        </div>
+
+        <div className="flex flex-wrap gap-[40px]">
+
+          <FormField
+            labelName="Goal *"
+            placeholder="ETH 0.50"
+            inputType="text"
+            value={form.target}
+            handleChange={(e) => handleFormChange('target', e)}
+          />
+          <FormField
+            labelName="End Date *"
+            placeholder="End Date"
+            inputType="date"
+            value={form.deadline}
+            handleChange={(e) => handleFormChange('deadline', e)}
+          />
+        </div>
+
+        <FormField
+            labelName="Campaign image *"
+            placeholder="Place image URL of your campaign"
+            inputType="url"
+            value={form.image}
+            handleChange={(e) => handleFormChange('image', e)}
+          />
+
+          <div className="flex justify-center items-center mt-[40px]">
+            <CustomButton
+              btnType="submit"
+              title="Submit new campaign"
+              styles="bg-[#1dc071]"
+            />
+          </div>
+      </form>
     </div>
   )
 }
