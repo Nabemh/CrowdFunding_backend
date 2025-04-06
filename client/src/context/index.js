edb12e236b90a06537a5d41f242fd8b7e81b3e30
Nabemh <nhanior@gmail.com>
2025-04-06 11:28:50 +0100

    call contract

--- client/src/context/index.js
+++ client/src/context/index.js
@@ -6,5 +6,6 @@ import { ethers } from 'ethers';
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-    const { contract } = useContract('0xb52d0fb62431e3ba90ec2ecde8c183044f4adac1')
+    const { contract } = useContract('0xb52d0fb62431e3ba90ec2ecde8c183044f4adac1');
+    const { mutateAsync: myFunction, isLoading, error } = useContractWrite(contract, "creareCampaign");
 }
