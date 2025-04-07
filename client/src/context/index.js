69e92e221f5b5271adf43a427ba7f24a41fe58c2
Nabemh <nhanior@gmail.com>
2025-04-07 21:35:29 +0100

    try

--- client/src/context/index.js
+++ client/src/context/index.js
@@ -8,4 +8,4 @@ const StateContext = createContext();
 export const StateContextProvider = ({ children }) => {
     const { contract } = useContract('0xb52d0fb62431e3ba90ec2ecde8c183044f4adac1');
     const { mutateAsync: myFunction, isLoading, error } = useContractWrite(contract, "creareCampaign");
-}
+} 
