9750ec85dafc5f9a30fe1c004a421565d2142f13
Nabemh <nhanior@gmail.com>
2025-04-09 19:45:17 +0100

    feat: state context

--- client/src/context/index.js
+++ client/src/context/index.js
@@ -12,4 +12,33 @@ export const StateContextProvider = ({ children }) => {
 
     const address = useAddress();
     const connect = useMetamask();
+
+    const publishCampaign = async (form) => {
+        try {
+            const data = await createCampaign([
+                address,
+                form.title,
+                form.description,
+                form.target,
+                new Date(form.deadline).getTime(),
+                form.image
+            ])
+
+            console.log("Contract call success", data)
+        } catch (error) {
+            console.log("Contract call failure", error)
+
+        }
+    }
+    return (
+        <StateContext.Provider
+            value={{
+                address,
+                contract,
+                createCampaign, publishCampaign,
+             }}
+        >
+            {children}
+        </StateContext.Provider>
+    )
 }
