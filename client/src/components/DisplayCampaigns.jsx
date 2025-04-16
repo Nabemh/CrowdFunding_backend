317364957d088ff143f17ce8a603ea8b2b31b33e
Nabemh <nhanior@gmail.com>
2025-04-16 10:47:50 +0100

    hangle navigations

--- client/src/components/DisplayCampaigns.jsx
+++ client/src/components/DisplayCampaigns.jsx
@@ -6,6 +6,10 @@ import { loader } from '../assets';
 const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
   const navigate = useNavigate();
 
+  const handleNavigate = (campaign) => {
+    navigate(`/campaign-details/${campaign.title}`, { state : campaign })
+  }
+
   return (
     <div>
       <h1 className="font-epilogue font-semibold text-[18px] text-white text-left">{title} ({campaigns.length})</h1>
@@ -16,10 +20,17 @@ const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
         <img src={loader} alt="loader" className="w-[100px] h-[100px] object-contain" />
       )}
 
-      {!isLoading && campaigns.length === 1 && (
+      {!isLoading && campaigns.length === 0 && (
         <p className='font-epilogue font-semibold text-[14px] leading-[30px] text-[#818183]'>
           You have not created any campaigns yet.
         </p>
+      )}
+
+      {!isLoading && campaigns.length > 0 && campaigns.map((campaign) => <FundCard 
+      key={campaign.id}
+      {...campaign}
+      handleClick={() => handleNavigate(campaign)}
+      />
       )}
     </div>
     </div>
