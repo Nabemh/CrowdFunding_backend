77002b3c4847909415a7c3b393fdb6e3374e7d56
Nabemh <nhanior@gmail.com>
2025-04-17 16:56:53 +0100

    fixes

--- client/src/components/DisplayCampaigns.jsx
+++ client/src/components/DisplayCampaigns.jsx
@@ -27,7 +27,7 @@ const DisplayCampaigns = ({ title, isLoading, campaigns }) => {
         </p>
       )}
 
-      {!isLoading && campaigns.length > 0 && campaigns.map((campaign) => <FundCard 
+      {!isLoading && campaigns.length > 0 && campaigns.map((campaign) => <FundCard
       key={campaign.id}
       {...campaign}
       handleClick={() => handleNavigate(campaign)}
