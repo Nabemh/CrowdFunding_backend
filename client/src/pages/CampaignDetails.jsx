ebd03198ad090bb9e29410bfcaed759c88cacc85
Nabemh <nhanior@gmail.com>
2025-04-18 20:47:10 +0100

    Donators

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -57,11 +57,24 @@ const CampaignDetails = () => {
             <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">Story</h4>
 
             <div className='mt-[20px]'>
-
+              <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">{state.description}</p>
             </div>
           </div>
 
+          <div>
+            <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">Donators</h4>
 
+            <div className='mt-[20px] flex flex-col gap-4'>
+              {donators.length > 0 ? donators.map((item, index) => {
+                <div>
+                  DONATOR
+                </div>
+              }) :   (
+                <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">No donators yet. Be the first one!</p>
+              )}
+
+            </div>
+          </div>
         </div>
     </div>
 
