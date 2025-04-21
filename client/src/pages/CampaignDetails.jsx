f7cdb83cc8871a2ace14aabb268c38346ab0447a
Nabemh <nhanior@gmail.com>
2025-04-21 16:25:44 +0100

    fix: donators list

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -84,18 +84,16 @@ const CampaignDetails = () => {
           <div>
             <h4 className="font-epilogue font-semibold text-[18px] text-white uppercase">Donators</h4>
 
-            <div className='mt-[20px] flex flex-col gap-4'>
-              {donators.length > 0 ? donators.map((item, index) => {
-                <div key={`${item.donator}-${index}`}
-                className='flex - justify-between items-center gap-4'>
-                  <p className='font-epilogue font-normal text-[16px] text-[#b2b3bd] leading-[26px] break-all'>{index + 1}. {item.donator}</p>
-                  DONATOR
-                </div>
-              }) :   (
-                <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">No donators yet. Be the first one!</p>
-              )}
-
-            </div>
+              <div className="mt-[20px] flex flex-col gap-4">
+                {donators.length > 0 ? donators.map((item, index) => (
+                  <div key={`${item.donator}-${index}`} className="flex justify-between items-center gap-4">
+                    <p className="font-epilogue font-normal text-[16px] text-[#b2b3bd] leading-[26px] break-ll">{index + 1}. {item.donator}</p>
+                    <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] break-ll">{item.donation}</p>
+                  </div>
+                )) : (
+                  <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">No donators yet. Be the first one!</p>
+                )}
+              </div>
           </div>
         </div>
 
