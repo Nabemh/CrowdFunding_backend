2dd0ef9730452ed807b0f9d80811741a3295c821
Nabemh <nhanior@gmail.com>
2025-04-22 17:07:52 +0100

    theme change init

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { logo, sun } from '../assets';
+import { logo, sun, moon } from '../assets';
 import { navlinks } from '../constants';
 import CampaignDetails from '../pages/CampaignDetails'
 
