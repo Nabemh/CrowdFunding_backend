2b4d9e44271184eced315a5abee6c7fabf37e7b6
Nabemh <nhanior@gmail.com>
2025-04-25 19:18:20 +0100

    theme change fix

--- client/src/components/FundCard.jsx
+++ client/src/components/FundCard.jsx
@@ -13,23 +13,23 @@ const FundCard = ({ owner, title, description, target, deadline, amountCollected
         <div className='flex flex-col p-4'>
             <div className='flex flex-row items-center mb-[18px]'>
                 <img src={tagType} alt="tag" className='w-[17px] h-[17px] object-contain'/>
-                <p className="ml-[12px] mt-[2px] font-epilogue font-medium text-[12px] text-[#808191]">Real Estate</p>
+                <p className="ml-[12px] mt-[2px] font-epilogue font-medium text-[12px] text-text">Real Estate</p>
             </div>
 
             <div className='block'>
                 <h3 className='font-epilogue font-semibold text-[16px] text-text text-left leading-[26px] truncate'>{title}</h3>
-                <p className='mt-[5px] font-epilogue font-normal text-[#808191] text-left leading-[18px] truncate'>{description}</p>
+                <p className='mt-[5px] font-epilogue font-normal text-text text-left leading-[18px] truncate'>{description}</p>
             </div>
 
             <div className='flex justify-between flex-wrap mt-[15px] gap-2'>
                 <div className='flex flex-col'>
-                    <h4 className='font-epilogue font-semibold text-[14px] text-[#b2b3bd] leading-[22px]'>{amountCollected}</h4>
-                    <p className='mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-[#808191] sm:max-w-[120px] truncate'>Raised of {target}</p>
+                    <h4 className='font-epilogue font-semibold text-[14px] text-text leading-[22px]'>{amountCollected}</h4>
+                    <p className='mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-text sm:max-w-[120px] truncate'>Raised of {target}</p>
                 </div>
 
                 <div className='flex flex-col'>
-                    <h4 className='font-epilogue font-semibold text-[14px] text-[#b2b3bd] leading-[22px]'>{remainingDays}</h4>
-                    <p className='mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-[#808191] sm:max-w-[120px] truncate'>Days Left</p>
+                    <h4 className='font-epilogue font-semibold text-[14px] text-text leading-[22px]'>{remainingDays}</h4>
+                    <p className='mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-text sm:max-w-[120px] truncate'>Days Left</p>
                 </div>
             </div>
 
@@ -38,7 +38,7 @@ const FundCard = ({ owner, title, description, target, deadline, amountCollected
                     <img src={thirdweb} alt='user' className='w-1/2 h-1/2 object-contain'/>
                     {/* make this to show a random image for the user like a bitmoji or something */}
                 </div>
-                <p className='flex-1 font-epilogue font-normal text-[12px] text-[#808191] truncate'>by <span className='text-[#b2b3bd]'>{owner}</span></p>
+                <p className='flex-1 font-epilogue font-normal text-[12px] text-text truncate'>by <span className='text-text'>{owner}</span></p>
 
             </div>
 
