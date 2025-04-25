60fcd92528ca7212684da069f94200676dd3d771
Nabemh <nhanior@gmail.com>
2025-04-25 17:35:34 +0100

    navbar light theme fix

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
 
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
-      <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
+      <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#F5F5F5] dark:bg-[#1c1c24] rounded-[100px]">
         <input type="text" placeholder="Search for campaigns" className="flex w-full font-epilogue font-normal text-[14px] placeholder:text-[#4b5264] text-white bg-transparent outline-none" />
 
         <div className="w-[72px] h-full rounded-[20px] bg-[#4acd8d] flex justify-center items-center cursor-pointer">
@@ -34,7 +34,7 @@ const Navbar = () => {
         />
 
         <Link to="/profile">
-          <div className='w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer'>
+          <div className='w-[52px] h-[52px] rounded-full bg-[#F5F5F5] dark:bg-[#2c2f32] flex justify-center items-center cursor-pointer'>
             <img src={thirdweb} alt="user" className='1-[60%] h-[60%] object-contain' />
           </div>
         </Link>
@@ -42,7 +42,7 @@ const Navbar = () => {
 
       {/* Mobile Navigation */}
       <div className="sm:hidden flex justify-between items-center relative">
-        <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer">
+        <div className="w-[40px] h-[40px] rounded-[10px] bg-[#F5F5F5] dark:bg-[#2c2f32] flex justify-center items-center cursor-pointer">
             <img src={logo} alt="user" className="w-[60%] h-[60%] object-contain" />
           </div>
 
