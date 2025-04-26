8f6d76d08b6d63151b115f698593693c0b9e5a23
Nabemh <nhanior@gmail.com>
2025-04-26 17:49:14 +0100

    fix

--- client/src/components/CountBox.jsx
+++ client/src/components/CountBox.jsx
@@ -4,7 +4,7 @@ const CountBox = ({ title, value }) => {
   return (
     <div className='flex flex-col items-center w-[150px]'>
         <h4 className='font-epilogue font-bold text-[30px] text-text p-3 bg-background rounded-t-[10px] w-full text-center truncate'>{value}</h4>
-        <p className='font-epilogue font-normal text-[16px] text-[#808191] bg-[#28282e] px-3 py-2 w-full rounded-b-[10px] text-center'>{title}</p>
+        <p className='font-epilogue font-normal text-[16px] text-[#808191] bg-background px-3 py-2 w-full rounded-b-[10px] text-center'>{title}</p>
 
     </div>
   )
--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -46,7 +46,7 @@ const CampaignDetails = () => {
       <div className='w-full flex md:flex-row flex-col mt-10 gap-[30px]'>
         <div className='flex-1 flex-col'>
           <img src={state.image} alt="campaign" className='w-full h-[410px] object-cover rounded-xl' />
-          <div className='relative w-full h-[5px] bg-[#3a3a43] mt-2 '>
+          <div className='relative w-full h-[5px] bg-background mt-2 '>
             <div className='absolute h-full bg-[#4acd8d]' style={{ width: `${calculateBarPercentage(state.target, state.amountCollected)}%`, maxWidth:'100%'}}>
             </div>
           </div>
@@ -65,7 +65,7 @@ const CampaignDetails = () => {
             <h4 className="font-epilogue font-semibold text-[18px] text-text uppercase">Creator</h4>
 
             <div className="mt-[20px] flex flex-row items-center flex-wrap gap-[14px]">
-              <div className="w-[52px] h-[52px] flex items-center justify-center rounded-full bg-[#2c2f32] cursor-pointer">
+              <div className="w-[52px] h-[52px] flex items-center justify-center rounded-full bg-[#28282e] cursor-pointer">
                 <img src={thirdweb} alt="user" className="w-[60%] h-[60%] object-contain"/>
               </div>
               <div>
@@ -89,8 +89,8 @@ const CampaignDetails = () => {
               <div className="mt-[20px] flex flex-col gap-4">
                 {donators.length > 0 ? donators.map((item, index) => (
                   <div key={`${item.donator}-${index}`} className="flex justify-between items-center gap-4">
-                    <p className="font-epilogue font-normal text-[16px] text-[#b2b3bd] leading-[26px] break-ll">{index + 1}. {item.donator}</p>
-                    <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] break-ll">{item.donation}</p>
+                    <p className="font-epilogue font-normal text-[16px] text-[#787878]  leading-[26px] break-all">{index + 1}. {item.donator}</p>
+                    <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] break-all">{item.donation}</p>
                   </div>
                 )) : (
                   <p className="font-epilogue font-normal text-[16px] text-[#808191] leading-[26px] text-justify">No donators yet. Be the first one!</p>
@@ -103,7 +103,7 @@ const CampaignDetails = () => {
           <h4 className="font-epilogue font-semibold text-[18px] text-text uppercase">Fund</h4>
 
           <div className='mt-[20px] flex flex-col p-4 bg-background rounded-[10px]'>
-            <p className='font-epilogue font-medium text-[20px] leading-[30px] text-center text-[#808191]'>
+            <p className='font-epilogue font-medium text-[20px] leading-[30px] text-center text-text'>
               Fund the campaign
             </p>
             <div className='mt-[30px]'>
@@ -116,7 +116,7 @@ const CampaignDetails = () => {
                   onChange={(e) => setAmount(e.target.value)}
                 />
 
-                <div className='my-[20px] p-4 bg-[#13131a] rounded-[10px]'>
+                <div className='my-[20px] p-4 bg-background rounded-[10px]'>
                   <h4 className='font-epilogue font-semibold text-[14px] leading-[22px] text-text'>
                     Back it because you believe in it.
                   </h4>
