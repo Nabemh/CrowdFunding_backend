d6610f397d241f4d5b6e2edec9ad4936a6aaaad9
Nabemh <nhanior@gmail.com>
2025-04-26 18:20:49 +0100

    logout init

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,6 +2,7 @@ import FormField from './FormField'
 import DisplayCampaigns from './DisplayCampaigns'
 import FundCard from './FundCard'
 import CountBox from './CountBox'
+import Logout from './Logout'
 export { default as Sidebar } from './Sidebar';
 export { default as Navbar } from './Navbar';
 export { default as CustomButton } from './CustomButton';
@@ -10,3 +11,4 @@ export { default as Loader } from './Loader';
 export { default as DisplayCampaigns } from './DisplayCampaigns';
 export { default as FundCard } from './FundCard';
 export { default as CountBox } from './CountBox';
+export { default as Logout } from './Logout';
