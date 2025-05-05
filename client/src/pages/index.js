71de4487affe49faf540f43b2d5aae9f4cbbcc54
Nabemh <nhanior@gmail.com>
2025-05-05 20:03:06 +0100

    init

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,4 +1,6 @@
+import Withdraw from './Withdraw'
 export { default as Home } from './Home';
 export { default as Profile } from './Profile';
 export { default as CreateCampaign } from './CreateCampaign';
 export { default as CampaignDetails } from './CampaignDetails';
+export { default as Withdraw } from './Withdraw';
