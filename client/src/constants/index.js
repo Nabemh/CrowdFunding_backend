d4a296f3845caf8979aa3ab014bdf5839408f9b4
Nabemh <nhanior@gmail.com>
2025-05-06 11:35:50 +0100

    fix

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -21,7 +21,7 @@ export const navlinks = [
     name: 'withdraw',
     imgUrl: withdraw,
     link: '/',
-    disabled: '/withdraw',
+    link: '/withdraw',
   },
   {
     name: 'profile',
@@ -32,6 +32,6 @@ export const navlinks = [
     name: 'logout',
     imgUrl: logout,
     link: '/',
-    disabled: true,
+    link: true,
   },
 ];
