d463092e46990db9ceb63de8cb6d462080932605
Nabemh <nhanior@gmail.com>
2025-05-15 20:50:55 +0100

    staging exports

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,6 +1,8 @@
 import Withdraw from './Withdraw'
+import AdminDetails from './AdminDetails'
 export { default as Home } from './Home';
 export { default as Profile } from './Profile';
 export { default as CreateCampaign } from './CreateCampaign';
 export { default as CampaignDetails } from './CampaignDetails';
 export { default as Withdraw } from './Withdraw';
+export { default as AdminDetails } from './AdminDetails';
